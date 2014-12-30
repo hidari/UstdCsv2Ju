55e855cd1b8dd62667373dd7bfc85cb8f2949a72
Hidari <ika5140@gmail.com>
2014-12-30 18:40:48 +0900

    ProductInfoのテスト追加

--- UstdCsv2JuTest/ProductInfoTest.cs
+++ UstdCsv2JuTest/ProductInfoTest.cs
@@ -1,6 +1,34 @@
-﻿namespace Hidari0415.UstdCsv2Ju.Tests
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Hidari0415.UstdCsv2Ju.Tests
 {
+	[TestFixture]
 	public class ProductInfoTest
 	{
+		private ProductInfo _sut;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_sut = new ProductInfo();
+		}
+
+		[Test]
+		public void GetProductInfomation()
+		{
+			_sut.Title.Is("UstdCsv2Ju");
+			_sut.Description.Is("");
+			_sut.Company.Is("hidari-lab.net");
+			_sut.Product.Is("UstdCsv2Ju");
+			_sut.Copyright.Is("Copyright Hidari0415 2014.");
+			_sut.Trademark.Is("");
+			_sut.VersionString.Is("0.1.1 β");
+			_sut.Libraries.IsStructuralEqual(new List<Library>
+			{
+				new Library("CSVHelper", new Uri("http://joshclose.github.io/CsvHelper/"))
+			});
+		}
 	}
 }
