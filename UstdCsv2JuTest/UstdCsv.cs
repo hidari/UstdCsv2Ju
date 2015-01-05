0cc1abb848c039b1b5d35642be5816189d9841b4
Hidari <ika5140@gmail.com>
2015-01-05 22:31:21 +0900

    ひとまず

---
+++ UstdCsv2JuTest/UstdCsv.cs
@@ -0,0 +1,7 @@
+﻿namespace Hidari0415.UstdCsv2Ju.Tests
+{
+	public class UstdCsv
+	{
+		public static readonly string NormalCsv = "Kind,Name,File,Value\r\nPublic Function,\"DoSomething(int, int)\",src\\module\\hoge.cpp,19\r\nPrivate Function,GetSomething(LPCTSTR),src\\module\\fuga.cpp,45";
+	}
+}
--- UstdCsv2JuTest/UstdCsvReaderTests.cs
+++ UstdCsv2JuTest/UstdCsvReaderTests.cs
@@ -1,8 +1,23 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
 
 namespace Hidari0415.UstdCsv2Ju.Tests
 {
 	public class UstdCsvReaderTests
 	{
+		[Test]
+		public void ReadNormalUstdCsv()
+		{
+			File.WriteAllText("Ustd.csv", UstdCsv.NormalCsv);
+			var actual = UstdCsvReader.ReadMetricRecords("Ustd.csv");
+			var expect = new List<MetricRecord>
+			{
+				new MetricRecord() {File = "src\\module\\hoge.cpp", Kind = "Public Function", Name = "DoSomething(int, int)", Value = 19},
+				new MetricRecord() {File = "src\\module\\fuga.cpp", Kind = "Private Function", Name = "GetSomething(LPCTSTR)", Value = 45}
+			};
+
+			actual.IsStructuralEqual(expect);
+		}
 	}
 }
